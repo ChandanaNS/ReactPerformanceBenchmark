e01936b929a31092695e6252b3fd55792321976b
ChandanaNS <10539269@mydbs.ie>
2020-11-26 00:27:13 +0000

    JS performance logs and click issue fix

--- Javascript/script.js
+++ Javascript/script.js
@@ -15,8 +15,8 @@ var todoList = {
   ],
   addTodos(event) {
     // press enter to create a new todo
+    var addTodoStart = performance.now();
     const addTodoTextInput = document.getElementById("textInput");
-    // if (event.keyCode === 13) {
     if (event.type === "keydown") {
       if (addTodoTextInput.value === "") {
         alert("Please add in a todo!");
@@ -30,41 +30,70 @@ var todoList = {
       addTodoTextInput.value = "";
       view.displayTodos();
     }
+    var addTodoEnd = performance.now();
+    console.log(
+      "addTodos::: Create new Todo:: " +
+        (addTodoEnd - addTodoStart) +
+        " milliseconds."
+    );
   },
-  changeTodos(position, newTodo) {
-    this.todos[position].todoText = newTodo;
-    view.displayTodos();
-  },
+  // changeTodos(position, newTodo) {
+  //   var changeTodoStart = performance.now();
+  //   this.todos[position].todoText = newTodo;
+  //   view.displayTodos();
+  //   var changeTodoEnd = performance.now();
+  //   console.log(
+  //     "changeTodos::: " + (changeTodoEnd - changeTodoStart) + " milliseconds."
+  //   );
+  // },
   deleteTodos(position) {
+    var deleteTodoStart = performance.now();
     this.todos.splice(position, 1);
     view.displayTodos();
+    var deleteTodoEnd = performance.now();
+    console.log(
+      "deleteTodos::: " + (deleteTodoEnd - deleteTodoStart) + " milliseconds."
+    );
   },
+
   toggleCompleted(position) {
+    var toggleCompletedStart = performance.now();
     this.todos[position].completed = !this.todos[position].completed;
     view.displayTodos();
+    var toggleCompletedEnd = performance.now();
+    console.log(
+      "toggleCompleted ::: " +
+        (toggleCompletedEnd - toggleCompletedStart) +
+        " milliseconds."
+    );
   },
-  // toggleAll() {
-  //   const totalTodos = this.todos.length;
-  //   let completedTodos = 0;
-  //   // get number of completed todos
+  toggleAll() {
+    var toggleAllStart = performance.now();
+    const totalTodos = this.todos.length;
+    let completedTodos = 0;
+    // get number of completed todos
 
-  //   this.todos.forEach(function (todo) {
-  //     // === true shorthand
-  //     if (todo.completed) {
-  //       completedTodos++;
-  //     }
-  //   });
-  //   // if everything is true make it false: toggles all completed to all uncompleted
-  //   this.todos.forEach(function (todo) {
-  //     if (totalTodos === completedTodos) {
-  //       todo.completed = false;
-  //       // otherwise make everything true: if some completed toggles all to completed
-  //     } else {
-  //       todo.completed = true;
-  //     }
-  //   });
-  //   view.displayTodos();
-  // },
+    this.todos.forEach(function (todo) {
+      // === true shorthand
+      if (todo.completed) {
+        completedTodos++;
+      }
+    });
+    // if everything is true make it false: toggles all completed to all uncompleted
+    this.todos.forEach(function (todo) {
+      if (totalTodos === completedTodos) {
+        todo.completed = false;
+        // otherwise make everything true: if some completed toggles all to completed
+      } else {
+        todo.completed = true;
+      }
+    });
+    view.displayTodos();
+    var toggleAllEnd = performance.now();
+    console.log(
+      "toggleAll:::" + (toggleAllEnd - toggleAllStart) + " milliseconds."
+    );
+  },
 };
 
 view = {
@@ -102,27 +131,27 @@ view = {
       }
       todoTextInput.value = todo.todoText;
       todoLi.appendChild(view.createDeleteButton());
-      // view.todosToday();
+      view.todosToday();
     });
   },
-  // todosToday() {
-  //   // count how many completed todos there are
-  //   completedTodos = 0;
-  //   todoList.todos.forEach(function (todo) {
-  //     if (todo.completed === false) {
-  //       completedTodos++;
-  //     }
-  //   });
-  //   const todosToday = document.getElementById("todosToday");
-  //   if (completedTodos > 1) {
-  //     todosToday.textContent = `you have ${completedTodos} tasks to complete today`;
-  //   } else if (completedTodos === 1) {
-  //     todosToday.textContent = "you have 1 more task to complete today";
-  //   } else {
-  //     todosToday.textContent =
-  //       "congratulations! you have no tasks to complete!";
-  //   }
-  // },
+  todosToday() {
+    // count how many completed todos there are
+    completedTodos = 0;
+    todoList.todos.forEach(function (todo) {
+      if (todo.completed === false) {
+        completedTodos++;
+      }
+    });
+    const todosToday = document.getElementById("todosToday");
+    if (completedTodos > 1) {
+      todosToday.textContent = `you have ${completedTodos} tasks to complete today`;
+    } else if (completedTodos === 1) {
+      todosToday.textContent = "you have 1 more task to complete today";
+    } else {
+      todosToday.textContent =
+        "congratulations! you have no tasks to complete!";
+    }
+  },
   createDeleteButton() {
     const createButton = document.createElement("button");
     createButton.innerHTML = '<i class="fas fa-times"></i>';
@@ -131,14 +160,19 @@ view = {
   },
   eventListeners() {
     // Onload event listener
-
+    var t2 = performance.now();
     const loadList = document.addEventListener("DOMContentLoaded", function (
       event
     ) {
       view.displayTodos();
     });
+    var t3 = performance.now();
+    console.log(
+      "EventListerner::: DOMContentLoaded :: " + (t3 - t2) + " milliseconds."
+    );
 
     // click to delete button
+    var t4 = performance.now();
     const deleteButton = document.addEventListener("click", function (event) {
       const deleteButtonId = event.target.parentNode.parentNode.id;
       if (event.target.parentNode.className === "deleteButton") {
@@ -146,14 +180,28 @@ view = {
         view.todosToday();
       }
     });
+    var t5 = performance.now();
+    console.log(
+      "EventListerner::: Click:: Delete :: " + (t5 - t4) + " milliseconds."
+    );
     // click to toggle
+    var t6 = performance.now();
     const clickToToggle = document.addEventListener("click", function (event) {
       const toggleId = event.target.parentNode.id;
-      if (event.target.nodeName === "I") {
+      if (
+        event.target.nodeName === "I" &&
+        !event.target.classList.contains("fa-times")
+      ) {
         todoList.toggleCompleted(toggleId);
       }
     });
+    var t7 = performance.now();
+    console.log(
+      "EventListerner::: Click:: Toggle row :: " + (t7 - t6) + " milliseconds."
+    );
+
     // click to edit and enter to save
+    var t6 = performance.now();
     const todoUl = document.querySelector("ul");
     todoUl.addEventListener("click", function (event) {
       const position = event.target.parentNode.id;
@@ -172,7 +220,14 @@ view = {
         });
       }
     });
+    var t7 = performance.now();
+    console.log(
+      "EventListerner::: Edit and save::" + (t7 - t6) + " milliseconds."
+    );
   },
 };
 
+var t0 = performance.now();
 view.eventListeners();
+var t1 = performance.now();
+console.log("Event Triggered ::: " + (t1 - t0) + " milliseconds.");
